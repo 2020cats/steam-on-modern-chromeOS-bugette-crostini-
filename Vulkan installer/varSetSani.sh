12f4b4bc14ddd842f5ad6d97c3c1f64ea981808e
2020cats <Andrewcwajbaum@gmail.com>
2026-01-10 20:01:17 -0800

    Update varSetSani.sh

--- Vulkan installer/varSetSani.sh
+++ Vulkan installer/varSetSani.sh
@@ -8,6 +8,9 @@ echo "Starting Sanitization..."
 mkdir -p ~/.config/systemd/user/cros-garcon.service.d/
 rm -f ~/.config/systemd/user/cros-garcon.service.d/*.conf
 
+# Defines var for image
+displayType="wayland,x"
+
 # Define the garcon config path so the script knows where to clean
 vulkanConfHome="$HOME/.config/systemd/user/cros-garcon.service.d/vulkan.conf"
 touch "$vulkanConfHome"
@@ -46,9 +49,9 @@ echo "Starting var config..."
     echo "XDG_SESSION_TYPE=wayland"
     echo "WAYLAND_DISPLAY=wayland-0"
     echo "XDG_RUNTIME_DIR=/run/user/$(id -u)"
-    echo "GDK_BACKEND=wayland"
+    echo "GDK_BACKEND=$displayType"
     echo "QT_QPA_PLATFORM=wayland"
-    echo "SDL_VIDEODRIVER=wayland,x11"
+    echo "SDL_VIDEODRIVER=$displayType"
     echo "DISABLE_WAYLAND_X11_INTEROP=0"
     echo "MESA_VK_DEVICE_SELECT=virtio"
 } | sudo tee -a /etc/environment > /dev/null
@@ -59,23 +62,25 @@ echo "Starting var config..."
     echo "export WAYLAND_DISPLAY=wayland-0"
     echo "export XDG_RUNTIME_DIR=/run/user/\$(id -u)"
     echo "export XDG_SESSION_TYPE=wayland"
-    echo "export GDK_BACKEND=wayland"
+    echo "export GDK_BACKEND=$displayType"
     echo "export QT_QPA_PLATFORM=wayland"
-    echo "export SDL_VIDEODRIVER=wayland,x11"
+    echo "export SDL_VIDEODRIVER=$displayType"
     echo "export DISABLE_WAYLAND_X11_INTEROP=0"
     echo "export MESA_VK_DEVICE_SELECT=virtio"
     echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu"
     echo "export STEAM_RUNTIME_PREFER_HOST_LIBRARIES=1"
+    echo "export VK_INSTANCE_LAYERS=VK_LAYER_MESA_device_select"
+    echo "export MESA_VK_DEVICE_SELECT=virtio"
 } >> ~/.bashrc
 
 # 5. Applying to CURRENT Session
 export VK_ICD_FILENAMES=$vulkanCompiledPath
 export WAYLAND_DISPLAY=wayland-0
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
-export SDL_VIDEODRIVER=wayland,x11
+export SDL_VIDEODRIVER=$displayType
 export DISABLE_WAYLAND_X11_INTEROP=0
 export XDG_SESSION_TYPE=wayland
-export GDK_BACKEND=wayland,x11
+export GDK_BACKEND=$displayType
 export WAYLAND_DISPLAY=wayland-0
 export STEAM_RUNTIME_PREFER_HOST_LIBRARIES=1
 
@@ -87,12 +92,15 @@ Environment="VK_ICD_FILENAMES=$vulkanCompiledPath"
 Environment="WAYLAND_DISPLAY=wayland-0"
 Environment="XDG_RUNTIME_DIR=/run/user/%U"
 Environment="XDG_SESSION_TYPE=wayland"
-Environment="GDK_BACKEND=wayland"
+Environment="GDK_BACKEND=$displayType"
 Environment="QT_QPA_PLATFORM=wayland"
-Environment="SDL_VIDEODRIVER=wayland,x11"
+Environment="SDL_VIDEODRIVER=$displayType"
 Environment="DISABLE_WAYLAND_X11_INTEROP=0"
 Environment="MESA_VK_DEVICE_SELECT=virtio"
 Environment="STEAM_RUNTIME_PREFER_HOST_LIBRARIES=1"
+Environment="SDL_VIDEODRIVER=$displayType"
+Environment="VK_INSTANCE_LAYERS=VK_LAYER_MESA_device_select"
+Environment="MESA_VK_DEVICE_SELECT=virtio"
 EOF
 
 if ! grep -q "\[Service\]" "$vulkanConfHome"; then
