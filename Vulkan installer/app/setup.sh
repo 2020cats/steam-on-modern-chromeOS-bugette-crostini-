2570d52778ca8aa1ae6f1b5f2704d04aad63a6a2
2020cats <Andrewcwajbaum@gmail.com>
2026-01-11 02:16:42 -0800

    Update setup.sh

--- Vulkan installer/app/setup.sh
+++ Vulkan installer/app/setup.sh
@@ -6,11 +6,11 @@ if ! command -v python3 >/dev/null 2>&1; then
   sudo apt install python3-tk
 fi
 
-APP_DIR="$HOME/Vulkan installer"
-ICON_NAME="vulkan-installer.png"
+APP_DIR="$HOME/Vulkan installer/app"
+ICON_NAME="icon.png"
 DESKTOP_FILE="vulkan-installer.desktop"
 
-sudo cp "$APP_DIR/assets/$ICON_NAME" /usr/share/icons/hicolor/48x48/apps/
+sudo cp "$APP_DIR/$ICON_NAME" /usr/share/icons/hicolor/48x48/apps/
 
 sudo cp "$APP_DIR/$DESKTOP_FILE" /usr/share/applications/
 
